043a29e4b9c1cbd54c29da0e74175168de669693
Phyks (Lucas Verney) <phyks@phyks.me>
2017-09-22 15:34:14 -0400

    Basic form for preferences

--- src/views/Preferences.vue
+++ src/views/Preferences.vue
@@ -1,6 +1,18 @@
 <template>
     <v-container fluid>
-        <p>TODO</p>
+        <v-form>
+            <v-layout row>
+                <v-flex xs12>
+                    <v-select
+                      label="I'm allergic to"
+                      v-bind:items="allergens"
+                      v-model="e6"
+                      multiple
+                      max-height="400"
+                    ></v-select>
+                </v-flex>
+            </v-layout>
+        </v-form>
     </v-container>
 </template>
 
@@ -8,6 +20,15 @@
 export default {
     created() {
         this.$store.dispatch('setTitle', { title: this.$t('navigation.preferences') });
+        this.$store.dispatch('setBackgroundColor', { backgroundColor: '#FF7043' });
+    },
+    data() {
+        return {
+            allergens: [
+                'nuts',
+                'milk',
+            ],
+        };
     },
 };
 </script>
